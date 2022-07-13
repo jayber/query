ececdf712bae576af0fd4f3ddfb1902d74fe9f8c
jayber <james.a.bromley@gmail.com>
2022-07-13 13:29:52 +0100

    first commit

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,48 @@ mod query;
 mod subclause;
 
 fn main() {
+    test_as_strings();
+
+    test_as_matching_records();
+}
+
+fn test_as_matching_records() {
+    let mut record = HashMap::new();
+    record.insert(String::from("name"), "bart".to_string());
+    record.insert(String::from("parent"), "homer".to_string());
+
+    assert!(Expr { field_name: "name".to_string(), operator: Equals, value: "bart".to_string() }.matches(&record));
+    assert!(!Expr { field_name: "name".to_string(), operator: Equals, value: "lisa".to_string() }.matches(&record));
+
+    assert!(Query::expression("name".to_string(), Equals, "bart".to_string()).matches(&record));
+    assert!(!Query::expression("name".to_string(), Equals, "lisa".to_string()).matches(&record));
+
+    assert!(Query::expression("name".to_string(), Equals, "bart".to_string())
+        .and(Expr { field_name: "name".to_string(), operator: Equals, value: "bart".to_string() }).matches(&record));
+    assert!(!Query::expression("name".to_string(), Equals, "bart".to_string())
+        .and(Expr { field_name: "name".to_string(), operator: Equals, value: "lisa".to_string() }).matches(&record));
+
+    assert!(Query::expression("name".to_string(), Equals, "bart".to_string())
+        .and(Query::expression("name".to_string(), Equals, "lisa".to_string())
+            .or(Expr { field_name: "name".to_string(), operator: Equals, value: "bart".to_string() })).matches(&record));
+
+    assert!(Query::expression("name".to_string(), Equals, "bart".to_string())
+        .or(Query::expression("name".to_string(), Equals, "lisa".to_string())
+            .and(Expr { field_name: "parent".to_string(), operator: Equals, value: "marge".to_string() })).matches(&record));
+
+    assert!(!Query::compound(
+        Query::bracketed_expression("name".to_string(), Equals, "bart".to_string())
+            .or(Query::expression("name".to_string(), Equals, "lisa".to_string())))
+        .and(Expr { field_name: "parent".to_string(), operator: Equals, value: "marge".to_string() }).matches(&record), "bracketing");
+
+    // test fails, behaves like every query is bracketed
+    assert!(Query::compound(
+        Query::expression("name".to_string(), Equals, "bart".to_string())
+            .or(Query::expression("name".to_string(), Equals, "lisa".to_string())))
+                .and(Expr { field_name: "parent".to_string(), operator: Equals, value: "marge".to_string() }).matches(&record), "non bracketing");
+}
+
+fn test_as_strings() {
     assert_eq!("app = slack",
                Query::expression("app".to_string(), Equals, "slack".to_string()).to_string());
     assert_eq!("app = slack AND (field1 = general OR field2 = chat)",
@@ -35,40 +77,6 @@ fn main() {
                            Query::bracketed_expression("field3".to_string(), Equals, "super".to_string())
                                .or(Expr { field_name: "field4".to_string(), operator: Equals, value: "pulvy".to_string() }))
                            .and(Expr { field_name: "fieldx".to_string(), operator: Equals, value: "z".to_string() }))).to_string());
-
-
-    let mut map = HashMap::new();
-    map.insert(String::from("name"), "bart".to_string());
-    map.insert(String::from("parent"), "homer".to_string());
-    assert!(Expr { field_name: "name".to_string(), operator: Equals, value: "bart".to_string() }.matches(&map));
-    assert!(!Expr { field_name: "name".to_string(), operator: Equals, value: "lisa".to_string() }.matches(&map));
-
-    assert!(Query::expression("name".to_string(), Equals, "bart".to_string()).matches(&map));
-    assert!(!Query::expression("name".to_string(), Equals, "lisa".to_string()).matches(&map));
-
-    assert!(Query::expression("name".to_string(), Equals, "bart".to_string())
-        .and(Expr { field_name: "name".to_string(), operator: Equals, value: "bart".to_string() }).matches(&map));
-    assert!(!Query::expression("name".to_string(), Equals, "bart".to_string())
-        .and(Expr { field_name: "name".to_string(), operator: Equals, value: "lisa".to_string() }).matches(&map));
-
-    assert!(Query::expression("name".to_string(), Equals, "bart".to_string())
-        .and(Query::expression("name".to_string(), Equals, "lisa".to_string())
-            .or(Expr { field_name: "name".to_string(), operator: Equals, value: "bart".to_string() })).matches(&map));
-
-    assert!(Query::expression("name".to_string(), Equals, "bart".to_string())
-        .or(Query::expression("name".to_string(), Equals, "lisa".to_string())
-            .and(Expr { field_name: "parent".to_string(), operator: Equals, value: "marge".to_string() })).matches(&map));
-
-    assert!(!Query::compound(
-        Query::bracketed_expression("name".to_string(), Equals, "bart".to_string())
-            .or(Query::expression("name".to_string(), Equals, "lisa".to_string())))
-        .and(Expr { field_name: "parent".to_string(), operator: Equals, value: "marge".to_string() }).matches(&map),"bracketing");
-
-    assert!(Query::compound(
-        Query::expression("name".to_string(), Equals, "bart".to_string())
-            .or(Query::expression("name".to_string(), Equals, "lisa".to_string())))
-        .and(Expr { field_name: "parent".to_string(), operator: Equals, value: "marge".to_string() }).matches(&map),"non bracketing");
-
 }
 
 
